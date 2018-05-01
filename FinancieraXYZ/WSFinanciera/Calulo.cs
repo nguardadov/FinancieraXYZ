c3b371322d04dd4bd8cd320d755bb774e6e4d8ed
nguardadov <nguardadov@gmail.com>
2018-05-01 11:48:58 -0600

    Calase calulo

---
+++ FinancieraXYZ/WSFinanciera/Calulo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+// clase que nos servira para obtener el monto que pagara
+namespace WSFinanciera
+{
+    public class Calulo
+    {
+        public double getCuota(double monto, int plazo, double interes)
+        {
+            // formula para calcular el pago que realizara en cada plazo
+            return monto * ((Math.Pow(1 + interes, plazo) * interes) / (Math.Pow(1 + interes, plazo) - 1));
+        }
+    }
+}
